2cc2c36880702eecaa8a31a794012cd71ecd4c36
Angel Delgado <xhopecli@protonmail.com>
2020-09-22 11:54:57 +0000

    Commit message

---
+++ Desktop/content/challengeOne1.js
@@ -0,0 +1,23 @@
+function largestOfFour(collection) {
+ // setting --- super array
+   let container = [];
+
+  collection.map(d =>{
+  	   /// getting max values for each array...
+  	    //  setting the variable with result inside...
+             let result = Math.max.apply(null, d);
+
+        // push the rrsult into super array 
+            container.push(result);
+       });
+    // return super array...
+        return container;
+}
+
+/// example values
+largestOfFour([
+	[4, 5, 1, 3],
+	[13, 27, 18, 26],
+	[32, 35, 37, 39],
+	[1000, 1001, 857, 1]
+	   ]);
